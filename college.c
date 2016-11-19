fa1e6706065e759c5e13affc04368fa272c2ed26
hashanadom <matbaranes@gmail.com>
2016-11-19 15:16:17 +0200

    Update college.c

--- college.c
+++ college.c
@@ -1,16 +1,32 @@
 
 typedef struct{
-short std_id;
-char[10] name;
-short age;
+  short std_id;
+  char[10] name;
+  short age;
 }students;
 
 typedef struct{
-short lctr_id;
-char[10] name;
-short age;
+  short lctr_id;
+  char[10] name;
+  short age;
 }lectureres;
 
 typedef struct{
-char[10] name;
+  char[10] name;
 }college;
+
+//function, given an instance of the struct, it's type ('s'-student,'l'-lectureres, 'c'-college), and it's size
+//saves the given data in binary format
+int save_data(struct *ptr, char type, int size_of){
+  FILE * file= fopen("output"+type, "wb");
+  int flag = 0;
+  
+  flag = (file != NULL);
+  (flag) ? fwrite(std_ptr, size_of, 1, file):0;
+  
+  fclose(file);
+
+  return flag;
+}
+
+void read_data
