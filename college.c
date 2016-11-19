72751c757cf2a683522ef0b308761077571344e8
hashanadom <matbaranes@gmail.com>
2016-11-19 14:33:38 +0200

    Create college.c

---
+++ college.c
@@ -0,0 +1,16 @@
+
+typedef struct{
+short std_id;
+char[10] name;
+short age;
+}students;
+
+typedef struct{
+short lctr_id;
+char[10] name;
+short age;
+}lectureres;
+
+typedef struct{
+char[10] name;
+}college;
